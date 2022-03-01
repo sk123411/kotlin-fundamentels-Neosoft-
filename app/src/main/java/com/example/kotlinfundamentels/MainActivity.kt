0a679fb3f90cc18d3c702256847ec79a7000b4f2
sk123411 <shivamk345@gmail.com>
2022-03-01 20:10:32 +0530

    worked on kotlin variable declarations and scope functions

--- app/src/main/java/com/example/kotlinfundamentels/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfundamentels/MainActivity.kt
@@ -2,10 +2,79 @@ package com.example.kotlinfundamentels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.kotlinfundamentels.model.Person
+
+
+const val TAG = "MainActivity"
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //var and val // const val
+        testVarVal()
+
+    }
+
+    private fun testVarVal() {
+
+        //USAGE OF CONST AND DEFINES COMPILE TIME CONSTANT
+        Log.d(TAG, " CONST VALUE.........")
+
+        //USAGE OF VAR
+        var name:String?= "Shubh Noriya"
+        Log.d(TAG, " VAR VALUE.........${name}")
+
+        //VAR DEFINES MUTABLE PROPERTY
+        name = "Raja Noriya"
+
+        Log.d(TAG, " VAR VALUE.........${name}")
+
+        //USAGE OF VAL
+        val vName:String = "Shubh Noriya"
+
+        //VAL DEFINES IMMUTABLE PROPERTY THUS VALUE CANNOT BE CHANGED
+        Log.d(TAG, " VAL VALUE.........${vName}")
+
+
+
+
+
+    }
+
+
+
+    fun testScopeFun(){
+
+        //USAGE OF APPLY AND ALSO FUNCTION
+        var person = Person(name = "Raju",age = 22,marriedStatus = "Single").apply {
+
+            age=33
+            marriedStatus="Married"
+        }.also {
+
+            Log.d(TAG, "Updated Age and Married Status ${it.age} ${it.marriedStatus}")
+
+        }
+
+        //USAGE OF LET FUNCTION
+        var letPerson=person.let {
+
+            it.name="Vinay"
+
+        }
+
+       var runPerson= person.run {
+
+
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/kotlinfundamentels/model/Person.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinfundamentels.model
+
+data class Person(var name:String,var age:Int,var marriedStatus:String)
+
+
